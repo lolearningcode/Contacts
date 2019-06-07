aa8ac4d57c93461e06301edabee2fd9a114bc0dc
Lo <lolearningcode@gmail.com>
2019-06-07 11:14:37 -0600

    completed assessment

--- Contacts/ContactDetailViewController.swift
+++ Contacts/ContactDetailViewController.swift
@@ -18,22 +18,35 @@ class ContactDetailViewController: UIViewController {
         didSet {
             loadViewIfNeeded()
             updateViews()
-
+            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         guard let name = nameTextField.text, !name.isEmpty, let phoneNumber = phoneNumberTextField.text, let emailAddress = emailAddressTextField.text else { return }
-        ContactController.shared.createContactWith(name: name, phoneNumber: phoneNumber, emailAddress: emailAddress) { (success) in
-            if success {
-                DispatchQueue.main.async {
-                    self.navigationController?.popViewController(animated: true)
+        
+        if let contact = contact {
+            ContactController.shared.update(contact: contact, withName: name, phoneNumber: phoneNumber, emailAddress: emailAddress) { (success) in
+                if success {
+                    DispatchQueue.main.async {
+                        self.navigationController?.popViewController(animated: true)
+                    }
+                }
+            }
+        } else {
+            let newContact = Contact(name: name, phoneNumber: phoneNumber, emailAddress: emailAddress)
+            ContactController.shared.createContactWith(name: name, phoneNumber: phoneNumber, emailAddress: emailAddress) { (success) in
+                if success {
+                    DispatchQueue.main.async {
+                        self.contact = newContact
+                        self.navigationController?.popViewController(animated: true)
+                    }
                 }
             }
         }
